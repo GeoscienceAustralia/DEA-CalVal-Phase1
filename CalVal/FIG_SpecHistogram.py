5f2307150fc66e963e0b2f30f3deb1307459d39e
Andrew Walsh <andrew.walsh@ga.gov.au>
2019-07-18 08:36:41 +1000

    Quash python warning in histogram plot

--- CalVal/FIG_SpecHistogram.py
+++ CalVal/FIG_SpecHistogram.py
@@ -10,15 +10,31 @@ import matplotlib.pyplot as plt
 #
 def FIG_spec_histogram(ls_ground_bands, s2_ground_bands, output, field_data, fignum):
 
-    fig_title = 'Figure '+str(fignum)+': '+field_data[0]+' '+field_data[1]+' '+field_data[2]+' '+field_data[3]
-    plt.tight_layout(pad=3.5, w_pad=1.0, h_pad=0.3)
-    
-    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(9.5, 9.5))
     if field_data[3] == 'Landsat8':
+        fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(9.5, 9.5))
+        plt.tight_layout(pad=3.5, w_pad=1.0, h_pad=1.3)
+        
         bands_only = ls_ground_bands.filter(like='band')
+        count = 0
+        for i in bands_only:
+            ls_ground_bands[i].hist(bins=50, ax=axes[int(count/3),count%3])
+            axes[int(count/3),count%3].set_title(i)
+            count += 1
+        axes[2,1].axis('off')
+        axes[2,2].axis('off')
+
     else:
+        fig, axes = plt.subplots(nrows=4, ncols=3, figsize=(9.5, 9.5))
+        plt.tight_layout(pad=3.5, w_pad=1.0, h_pad=1.3)
+        
         bands_only = s2_ground_bands.filter(like='band')
-    bands_only.hist(bins=50, ax=axes)
-    #fig.suptitle(fig_title+': Histograms by band for individual spectra', fontweight='bold')
+        count = 0
+        for i in bands_only:
+            s2_ground_bands[i].hist(bins=50, ax=axes[int(count/3),count%3])
+            axes[int(count/3),count%3].set_title(i)
+            count += 1
+        axes[3,2].axis('off')
+
+    fig_title = 'Figure '+str(fignum)+': '+field_data[0]+' '+field_data[1]+' '+field_data[2]+' '+field_data[3]
 
     plt.savefig(output+field_data[0]+'_'+field_data[1]+'_'+field_data[2]+'_'+field_data[3]+'_'+'Fig'+str(fignum)+'_BandHistograms.png')
