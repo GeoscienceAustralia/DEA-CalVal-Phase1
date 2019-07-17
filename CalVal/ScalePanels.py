ddfc4a798991d85b8abcc72f960b9576530bcb0a
Andrew Walsh <andrew.walsh@ga.gov.au>
2019-07-17 16:51:23 +1000

    Remove python pandas copy onto dataframe warning

--- CalVal/ScalePanels.py
+++ CalVal/ScalePanels.py
@@ -3,8 +3,16 @@ def scale_panels(slope, intercept, coszenith, gpt, good_panels, field_data):
     if field_data[5] == 'Radiance':
         ratio = gpt['Averaged_Panels']/(slope*(coszenith)+intercept)
 
+        PanCount = []
+    
         for i in range(len(gpt['Averaged_Panels'])):
-            good_panels.radiance[good_panels['date_saved'] == gpt['date_saved'].iloc[i]] = \
-                good_panels.radiance[good_panels['date_saved'] == gpt['date_saved'].iloc[i]]/ratio.iloc[i]
+            for j in range(len(good_panels[good_panels.date_saved == gpt['date_saved'].iloc[i]])):
+                PanCount.append(ratio.iloc[i])
+    
+        good_panels.radiance = good_panels.radiance/PanCount
+    
+#        for i in range(len(gpt['Averaged_Panels'])):
+#            good_panels.radiance[good_panels['date_saved'] == gpt['date_saved'].iloc[i]] = \
+#                good_panels.radiance[good_panels['date_saved'] == gpt['date_saved'].iloc[i]]/ratio.iloc[i]
 
     return good_panels
